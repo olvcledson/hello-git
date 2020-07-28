96bef8c3169af3add2f69f8bed8e46591326f028
inmetrics <oliveira.bnhc@gmail.com>
2020-07-28 00:53:04 -0300

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> nomes = new ArrayList<>();
+		
+		nomes.add("Cledson");
+		nomes.add("Cleber");
+		nomes.add("Kleryston");
+				
+		System.out.println("Peaky Blinders: " + nomes);
+		
+		}
+}
